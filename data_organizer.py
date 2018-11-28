28dc0988a0c28ded664992c1652eda231e7c0c42
sym19940907 <sym19940907@users.noreply.github.com>
2018-11-28 14:30:51 -0500

    added vgg training

--- data_organizer.py
+++ data_organizer.py
@@ -1,16 +1,33 @@
 import argparse
-import os
+import os, random
+
+s = set()
+flag = False
+for i in range(300):
+    for i in random.sample(range(5000), 10):
+        if i in s:
+            print(len(s))
+            flag = True
+            break
+        s.add(i)
+    if flag:
+        break
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--src")
+    parser.add_argument("--src_1")
+    parser.add_argument("--src_2")
     parser.add_argument("--dst")
-    parser.add_argument("--num", type=int)
+    parser.add_argument("--num_train", type=int)
+    parser.add_argument("--num_test", type=int)
+    parser.add_argument("--class_1")
+    parser.add_argument("--class_2")
     args = parser.parse_args()
     src_dir = args.src
     dst_dir = args.dst
     num = args.num
-    
+
     for img in os.listdir(src_dir):
         if img.endswith(".jpg"):
             src_img = os.path.join(src_dir, img)
@@ -19,4 +36,4 @@ if __name__ == "__main__":
             num -= 1
             if num <= 0:
                 break
-    
+#
