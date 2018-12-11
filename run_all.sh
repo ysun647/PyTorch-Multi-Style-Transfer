a28585fac5a7d6bffc47fbd40e94c84eb6fa8f18
Anlu Chen <anlu@Anlus-MacBook-Pro.local>
2018-12-10 22:57:17 -0500

    alter the order of bash

--- run_all.sh
+++ run_all.sh
@@ -25,6 +25,14 @@ sudo python inception.py --epochs 4800 \
 # =========
 
 sudo python inception.py --epochs 80 \
+ --traditional_aug 0 \
+ --neural_aug 1 \
+ --log_save_dir ./t_0_n_1_pretrained_tuneall/log \
+ --model_save_dir ./t_0_n_1_pretrained_tuneall/model \
+ --feature_extract 0 \
+ --save_step 1
+
+sudo python inception.py --epochs 80 \
  --traditional_aug 1 \
  --neural_aug 1 \
  --log_save_dir ./t_1_n_1_pretrained_tuneall/log \
@@ -39,14 +47,6 @@ sudo python inception.py --epochs 4800 \
  --model_save_dir ./t_1_n_0_pretrained_tuneall/model \
  --save_step 160 \
  --feature_extract 0
-
-sudo python inception.py --epochs 80 \
- --traditional_aug 0 \
- --neural_aug 1 \
- --log_save_dir ./t_0_n_1_pretrained_tuneall/log \
- --model_save_dir ./t_0_n_1_pretrained_tuneall/model \
- --feature_extract 0 \
- --save_step 1
 
 sudo python inception.py --epochs 4800 \
  --traditional_aug 0 \
