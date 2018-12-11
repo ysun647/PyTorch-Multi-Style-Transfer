1c795abb2ad054fa897a405fe03f27a3f0f79d4a
ysun647 <yimingsun1994@gmail.com>
2018-12-10 23:22:54 -0500

    changed order

--- run_all.sh
+++ run_all.sh
@@ -24,14 +24,6 @@ sudo python inception.py --epochs 4800 \
 
 # =========
 
-sudo python inception.py --epochs 4800 \
- --traditional_aug 1 \
- --neural_aug 0 \
- --log_save_dir ./t_1_n_0_pretrained_tuneall/log \
- --model_save_dir ./t_1_n_0_pretrained_tuneall/model \
- --save_step 160 \
- --feature_extract 0
-
 sudo python inception.py --epochs 30 \
  --traditional_aug 0 \
  --neural_aug 1 \
@@ -39,6 +31,14 @@ sudo python inception.py --epochs 30 \
  --model_save_dir ./t_0_n_1_pretrained_tuneall/model \
  --feature_extract 0 \
  --save_step 1
+
+sudo python inception.py --epochs 4800 \
+ --traditional_aug 1 \
+ --neural_aug 0 \
+ --log_save_dir ./t_1_n_0_pretrained_tuneall/log \
+ --model_save_dir ./t_1_n_0_pretrained_tuneall/model \
+ --save_step 160 \
+ --feature_extract 0
 
 sudo python inception.py --epochs 4800 \
  --traditional_aug 0 \
