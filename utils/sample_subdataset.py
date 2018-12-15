a8a43ebed0e64c1ecbfbe21bbc713846ab5a0b9a
ysun647 <yimingsun1994@gmail.com>
2018-12-15 01:00:46 -0500

    added sample subdataset

---
+++ utils/sample_subdataset.py
@@ -0,0 +1,33 @@
+import os
+import argparse
+import random
+
+if __name__ == '__main__':
+    '''
+    sample usage:
+    python sample_subdataset.py \
+        --src /data/<source-name> \
+        --dst /data/<some-target-name> \
+        --sample_ratio 0.5 \
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--src", type=str)
+    parser.add_argument("--dst", type=str)
+    parser.add_argument("--sample_ratio", type=float, default=0.5)
+    parser.add_argument("--suffix", type=str, default='png')
+    args = parser.parse_args()
+    
+    assert (0 < args.sample_ratio < 1)
+    
+    os.makedirs(args.dst, exist_ok=True)
+    
+    for img in args.src:
+        if not img.endswith(parser.suffix):
+            continue
+            
+        rand = random.random()
+        if rand < args.sample_ratio:
+            os.system("sudo cp {src_img} {tgt_img}".format(
+                src_img=os.path.join(args.src, img),
+                tgt_img=os.path.join(args.dst, img)
+            ))
