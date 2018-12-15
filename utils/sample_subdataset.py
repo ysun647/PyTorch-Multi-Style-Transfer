2712dc9d651a4fbcd92b34433ab0549ea0f00bd4
ysun647 <yimingsun1994@gmail.com>
2018-12-15 14:16:30 -0500

    updated sample_subdataset for debug

--- utils/sample_subdataset.py
+++ utils/sample_subdataset.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     parser.add_argument("--src", type=str)
     parser.add_argument("--dst", type=str)
     parser.add_argument("--sample_ratio", type=float, default=0.5)
-    parser.add_argument("--suffix", type=str, default='png')
+    parser.add_argument("--suffix", type=str, default='jpg')
     args = parser.parse_args()
     
     assert (0 < args.sample_ratio < 1)
@@ -22,11 +22,13 @@ if __name__ == '__main__':
     os.makedirs(args.dst, exist_ok=True)
     
     for img in args.src:
-        if not img.endswith(parser.suffix):
+        if not img.endswith(args.suffix):
+            print("found non {} file!".format(args.suffix))
             continue
             
         rand = random.random()
         if rand < args.sample_ratio:
+            print("got it!")
             os.system("sudo cp {src_img} {tgt_img}".format(
                 src_img=os.path.join(args.src, img),
                 tgt_img=os.path.join(args.dst, img)
